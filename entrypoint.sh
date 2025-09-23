cdfdb6d042edbfb2c174b15a0f5fd94e43f11424
omejiariv <145706652+omejiariv@users.noreply.github.com>
2025-09-22 19:43:02 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
 
+# Aumenta el límite de inotify para el monitoreo de archivos
+echo fs.inotify.max_user_watches=524288 | tee -a /etc/sysctl.conf && sysctl -p
+
+# Ejecuta el comando de inicio de Streamlit
+exec streamlit run app.py
